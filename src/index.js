25e885847627d266429a0f15e900129849b7a26c
unknown <muhamed.webdevelopment@gmail.com>
2021-05-06 01:10:51 +0200

    Initial App

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+  <h2>Hello React!</h2>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
